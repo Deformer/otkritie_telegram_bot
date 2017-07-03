2d2279dc987e3b7236934ccd882ec7babd1528ea
Unknown <serg19-03@mail.ru>
2017-07-03 19:53:09 +0300

    success request and format response

--- nl_analysis.js
+++ nl_analysis.js
@@ -2,18 +2,42 @@
  * Created by sergey on 03.07.17.
  */
 
-const yandex_speech = require('yandex-speech');
+const uuidv4 = require('uuid/v4');
+const yandexSpeech = require('yandex-speech');
 const config = require('./config');
+const parseString = require('xml2js').parseString;
 
-yandex_speech.ASR({
+const formatSuccessResponse = xml => new Promise((resolve, reject) => {
+  parseString(xml, (err, result) => {
+    if (err) return resolve(err);
+    const response = {
+      success: parseInt(result.recognitionResults.$.success, 10),
+      results: parseInt(result.recognitionResults.$.success, 10) === 1 ?
+        result.recognitionResults.variant.map(el => ({
+          text: el._,
+          confidence: el.$.confidence,
+        })) :
+        null,
+    };
+
+    resolve(response);
+  });
+});
+
+require('fs').createReadStream('public/how_to_reestablish_pin.mp3').pipe(yandexSpeech.ASR({
   developer_key: config.yandexSpeechKey,  // get in Yandex Developer Center
-  file: 'data/1.mp3', // check format
+  // file: 'public/how_to_reestablish_pin.mp3', // check format
+  uuid: uuidv4().replace(/-/g, ''),    // UUID without hyphens
+  topic: 'queries',  // ['queries', 'maps', 'notes', 'music']
   lang: 'ru-RU',      // ['ru-RU', 'tr-TR'],
+  filetype: 'audio/x-mpeg-3',  // ['audio/x-speex', 'audio/x-pcm;bit=16;rate=8000', 'audio/x-pcm;bit=16;rate=16000', 'audio/x-alaw;bit=13;rate=8000', 'audio/x-wav', 'audio/x-mpeg-3']
 }, (err, httpResponse, xml) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(httpResponse.statusCode, xml);
+    console.log(httpResponse.statusCode);
+    formatSuccessResponse(xml).then((resp) => {
+      console.log(resp);
+    });
   }
-},
-);
+}));
