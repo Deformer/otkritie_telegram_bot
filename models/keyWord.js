b0a6a0289448e9f41fc67641b2e5f059e28d7940
Unknown <serg19-03@mail.ru>
2017-07-05 15:57:26 +0300

    fix database models

--- models/keyWord.js
+++ models/keyWord.js
@@ -1,7 +1,7 @@
 const connection = require('./index');
 const Sequelize = require('sequelize');
 
-const KeyWord = connection.define('keyWord', {
+const KeyWord = connection.define('key_word', {
   id: {
     type: Sequelize.INTEGER,
     allowNull: false,
