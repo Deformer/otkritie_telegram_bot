dc49ff31a76c1505696b3bf506eed514ff301b7e
Unknown <serg19-03@mail.ru>
2017-07-06 11:46:24 +0300

    small refactor bot_server.js

--- bot_server.js
+++ bot_server.js
@@ -1,42 +1,41 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '394893817:AAGz0IEah-fBtSJDqmdbc8kAeo__XbkZfB0';
-const bot = new TelegramBot(token, {polling: true});
-
 const request = require('request');
 const fs = require('fs');
 
+const config = require('./config');
 
-let audioFileArray = [];
+const bot = new TelegramBot(config.telegramToken, { polling: true });
+
+const audioFileArray = [];
 
 module.exports = {
-    init: function () {
-        bot.on('message', (msg) => {
+  init() {
+    bot.on('message', (msg) => {
             // console.log(msg);
-            if (msg.voice) {
-                let fileId = msg.voice.file_id;
-                bot.getFile(fileId).then(response =>{
-                    let filePath = response.file_path;
-                    bot.getFileLink(fileId).then(fileLink => {
-                        let newFile = fs.createWriteStream(`./resources/${filePath}`);
-                        request(fileLink).pipe(newFile);
+      if (msg.voice) {
+        const fileId = msg.voice.file_id;
+        bot.getFile(fileId).then((response) => {
+          const filePath = response.file_path;
+          bot.getFileLink(fileId).then((fileLink) => {
+            const newFile = fs.createWriteStream(`./resources/${filePath}`);
+            request(fileLink).pipe(newFile);
 
-                        let fileMeta = {chatId: msg.chat.id, fileId: fileId, fileLink: fileLink};
-                        audioFileArray.push(fileMeta);
-                        console.log(`New voice message received: ${fileId}`);
-                    });
-                });
-            }
-            else if (msg.entities && msg.entities[0].type === 'bot_command') {
-                let fullCommand = msg.text;
-                console.log(`New bot command received: ${fullCommand}`);
-                switch (fullCommand) {
-                    case '/print': {
-                        audioFileArray.forEach(file => {
-                            bot.sendMessage(file.chatId, `Link file: ${file.fileLink}`);
-                        })
-                    }
-                }
-            }
+            const fileMeta = { chatId: msg.chat.id, fileId, fileLink };
+            audioFileArray.push(fileMeta);
+            console.log(`New voice message received: ${fileId}`);
+          });
         });
-    }
+      } else if (msg.entities && msg.entities[0].type === 'bot_command') {
+        const fullCommand = msg.text;
+        console.log(`New bot command received: ${fullCommand}`);
+        switch (fullCommand) {
+          case '/print': {
+            audioFileArray.forEach((file) => {
+              bot.sendMessage(file.chatId, `Link file: ${file.fileLink}`);
+            });
+          }
+        }
+      }
+    });
+  },
 };
--- config/index.js
+++ config/index.js
@@ -4,6 +4,7 @@
 module.exports = {
   port: 5030,
   yandexSpeechKey: '1fb760dc-6bd6-4936-bfa5-eb83e5db14ac',
+  telegramToken: '394893817:AAGz0IEah-fBtSJDqmdbc8kAeo__XbkZfB0',
   dbSettings: {
     database: 'telegram',
     user: 'admin',
